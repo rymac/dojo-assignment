5c523ff4915b26dea5181a042d60f13810e759c3
Ryosuke Machida <machmach@mac.com>
2018-09-20 20:16:29 +0900

    minor update

--- software-2/json_to_csv.py
+++ software-2/json_to_csv.py
@@ -3,8 +3,12 @@ import os
 import json
 
 def json_file_to_csv_string(json_file):
-    with open(json_file, "r") as f:
-        json_dict = json.load(f)
+    if os.path.exists(json_file):
+        with open(json_file, "r") as f:
+            json_dict = json.load(f)
+    else:
+        print("Error: No file at %s" % json_file)
+        sys.exit()
 
     csv_str = ('ITEM_NUMBER, DEFINE, REPAIR, PREVENT\n')
     for (i, item) in enumerate(json_dict):
@@ -18,8 +22,10 @@ def main():
     if argc != 2:
         print("Usage: python3 %s json_file" % sys.argv[0])
         sys.exit()
+    else:
+        json_file = sys.argv[1]
 
-    csv_str = json_file_to_csv_string(sys.argv[1])
+    csv_str = json_file_to_csv_string(json_file)
     print(csv_str)
 
 
