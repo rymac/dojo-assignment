ce74e3ab55895f6e8bb692f93d63b742bfeded45
Ryosuke Machida <ryosuke.machida@tri-ad.global>
2018-10-18 19:19:32 +0900

    Make FearConverter class and refactor code.

--- software-2/json_to_csv.py
+++ software-2/json_to_csv.py
@@ -1,35 +1,50 @@
 import sys
 import json
 
-def convert_json_file_to_csv_string(json_file):
-    json_dict = open_json_file(json_file)
-    csv_str = generate_csv_from_json(json_dict)
-    return csv_str
+class FearConverter:
+    def __init__(self):
+        self.HEADER_CSV = 'ITEM_NUMBER, DEFINE, PREVENT, REPAIR\n'
+        self.result_csv = ""
+        self.fears_json = {}
 
 
-def open_json_file(json_file):
-    try:
-        return read_json_dict_from_json_file(json_file)
-    except Exception as e:
-        print_error_and_exit(e)
+    def convert_json_file_to_csv_string(self, json_file):
+        self._open_json_file(json_file)
+        self._generate_csv_from_json()
+        return self.result_csv
 
 
-def read_json_dict_from_json_file(json_file):
-    with open(json_file, "r") as f:
-        json_dict = json.load(f)
-    return json_dict
+    def _open_json_file(self, json_file):
+        try:
+            return self._read_fears_from_json_file(json_file)
+        except Exception as e:
+            self._print_error_and_exit(e)
 
 
-def print_error_and_exit(e):
-    print("Error:", e)
-    sys.exit()
+    def _read_fears_from_json_file(self, json_file):
+        with open(json_file, "r") as f:
+            self.fears_json = json.load(f)
 
 
-def generate_csv_from_json(json_dict):
-    csv_str = ('ITEM_NUMBER, DEFINE, PREVENT, REPAIR\n')
-    for (i, item) in enumerate(json_dict):
-        csv_str += '%d, "%s", "%s", "%s"\n' % (i+1, item['DEFINE'], item['PREVENT'], item['REPAIR'])
-    return csv_str
+    def _print_error_and_exit(self, e):
+        print("Error:", e)
+        sys.exit()
+
+
+    def _generate_csv_from_json(self):
+        self._add_csv_header()
+        self._add_csv_body()
+
+
+    def _add_csv_header(self):
+        self.result_csv += self.HEADER_CSV
+
+
+    def _add_csv_body(self):
+        for (i, fear) in enumerate(self.fears_json):
+            fear_csv = '%d, "%s", "%s", "%s"\n' \
+                       % (i+1, fear['DEFINE'], fear['PREVENT'], fear['REPAIR'])
+            self.result_csv += fear_csv
 
 
 def main():
@@ -40,7 +55,8 @@ def main():
     else:
         json_file = sys.argv[1]
 
-    csv_str = convert_json_file_to_csv_string(json_file)
+    fear_converter = FearConverter()
+    csv_str = fear_converter.convert_json_file_to_csv_string(json_file)
     print(csv_str)
 
 
